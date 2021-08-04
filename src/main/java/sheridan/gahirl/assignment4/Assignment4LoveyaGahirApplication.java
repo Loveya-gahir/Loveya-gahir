ec70d0f68cf49aa0ea1d00176579cd9584959442
Loveya Gahir <gahirl@sheridancollege.ca>
2021-08-04 15:49:01 -0400

    Initial Commit and schema.sql + data.sql

---
+++ src/main/java/sheridan/gahirl/assignment4/Assignment4LoveyaGahirApplication.java
@@ -0,0 +1,13 @@
+package sheridan.gahirl.assignment4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment4LoveyaGahirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment4LoveyaGahirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sheridan/gahirl/assignment4/Assignment4LoveyaGahirApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.gahirl.assignment4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment4LoveyaGahirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
